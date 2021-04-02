55e88dd09248ef9fd164610c0c4dc15024d3fb05
Yudi Rosandi <rosandi@gmail.com>
2021-04-03 00:29:37 +0700

    on progress: fully RasPI device

--- seismoads.py
+++ seismoads.py
@@ -2,11 +2,13 @@
 
 import time
 import Adafruit_ADS1x15
+from gpiozero import Button
 
 chn=1 # channel mask! not number
 gain=1
 devchan=4
 adc=None
+trigpin=None
 
 def channelnum(cmask=None):
     # returns device maximum channel
@@ -18,60 +20,73 @@ def channelnum(cmask=None):
     for m in (1,2,4,8,16,32):
         if cmask & m:
             nchan+=1
-
+    if nchan > devchan:
+        nchan=devchan
     return nchan
 
 def chanlist(cmask):
-     for m in (1,2,4,8,16,32,64,128):
+    chlst=[]
+    c=0
+    for m in (1,2,4,8,16,32,64,128):
         if cmask & m:
-            nchan
+            chlst.append(c)
+        c+=1
    
+   return chlst
 
 def deviceInit(port='ADS1115',speed=None):
-    global adc
+    global adc,trigpin
+    
+    trigpin=Button(4)
+    
     if port is 'ADS1115':
         adc = Adafruit_ADS1x15.ADS1115()
     else
         adc = Adafruit_ADS1x15.ADS1015()
     
 
-def deviceClose():
-    pass
-
 def readadc(n):
-    nchan=channelnum(chn)
+    global chn,gain
+    cl=chanlist(chn)
     vals=[]
+    tstart=time.time()
     for i in range(n):
-        for ch in range(nchan):
+        for ch in cl:
             vals.append(adc.read_adc(ch,gain))
-    return 
-        
+    tend=time.time()
+    
+    return tend-tstart,cl
 
 def deviceCommand(scmd):
-    global chn, gain
+    global chn
     
     nchan=channelnum(chn)
     
     if scmd.find('msr')==0:
         ndata=int(scmd.split()[1])
-        
-        for i in range(nchan):
-            x=adc.read_adc(gain)
-        return 1000, createsignal(nchan), chn
+        dt,vals=readadc(nchan)
+        return dt, vals, chn
+
     elif scmd.find('trig')==0:
-        sleep(5)
-        print("trigger simulated")
-        return 1000, createsignal(nchan), chn
+        # WARNING: blocking!
+        trigpin.wait_for_press()
+        dt,vals=readadc(nchan)
+        return dt, vals, chn
         
     elif scmd.find('chn')==0:
         chn=int(scmd.split()[1])
-        return "dummy channel_mask {}".format(chn)
+        nchan=channelnum(chn)
+        return "channel mask: {} number {}".format(chn,chan)
     else:
-        return "dummy serial device"
+        return "ADS1x15 interface"
     
 def directMeasure():
-    return 1000, [1]*50, chn
+    return readadc(1)
     
 def clearQueue():
+    pass
+
+
+def deviceClose():
     pass
 
