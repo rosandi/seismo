e63765b7542d052254fae56958302ce0ef9d8a72
Yudi Rosandi <rosandi@gmail.com>
2021-04-02 22:05:09 +0700

    minor

--- seismoads.py
+++ seismoads.py
@@ -2,42 +2,61 @@
 
 import time
 import Adafruit_ADS1x15
-from random import random
-from math import pi,sin,exp
 
-chn=1
+chn=1 # channel mask! not number
+gain=1
+devchan=4
+adc=None
 
-def createsignal(nch):
-    nd=500
-    f=20.0
-    v=[]
-    theta=[nd*random() for xx in range(nch)]
+def channelnum(cmask=None):
+    # returns device maximum channel
+    if cmask is None:
+        return devchan
     
-    for i in range(nd):
-        for th in theta:
-            a=2.0*pi*f*i/nd
-            v.append(sin(a)*exp(-(0.001*(i-th)**2)))
-    return v
-
-def channelnum(cmask):
+    # or translate cmask to number of channels
     nchan=0
     for m in (1,2,4,8,16,32):
         if cmask & m:
             nchan+=1
+
     return nchan
 
-def deviceInit(port,speed):
-    pass
+def chanlist(cmask):
+     for m in (1,2,4,8,16,32,64,128):
+        if cmask & m:
+            nchan
+   
+
+def deviceInit(port='ADS1115',speed=None):
+    global adc
+    if port is 'ADS1115':
+        adc = Adafruit_ADS1x15.ADS1115()
+    else
+        adc = Adafruit_ADS1x15.ADS1015()
+    
 
 def deviceClose():
     pass
-    
+
+def readadc(n):
+    nchan=channelnum(chn)
+    vals=[]
+    for i in range(n):
+        for ch in range(nchan):
+            vals.append(adc.read_adc(ch,gain))
+    return 
+        
+
 def deviceCommand(scmd):
-    global chn
+    global chn, gain
     
     nchan=channelnum(chn)
     
     if scmd.find('msr')==0:
+        ndata=int(scmd.split()[1])
+        
+        for i in range(nchan):
+            x=adc.read_adc(gain)
         return 1000, createsignal(nchan), chn
     elif scmd.find('trig')==0:
         sleep(5)
