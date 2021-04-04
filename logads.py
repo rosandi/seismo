a6dc8cfde35e4d031586d8e585e60eb081617445
Yudi Rosandi <rosandi@gmail.com>
2021-04-04 20:27:41 +0700

    minor

--- logads.py
+++ logads.py
@@ -11,11 +11,14 @@ blocklen=1024
 run=True
 nkill=0
 bcnt=0
+nb=None
 
 for arg in sys.argv:
     if arg.find('block=') == 0:
         blocklen=int(arg.replace('block=',''))
         print('block length: %d'%(blocklen), file=sys.stderr)
+    if arg.fing('N=') == 0:
+        nb=int(arg.replace('N=',''))
 
 sa.deviceInit()
 
@@ -42,3 +45,8 @@ while run:
     for i in range(blocklen):
         print('{0} {1:>6} {2:>6} {3:>6} '.format(i,d[0,i],d[1,i],d[2,i]))
     
+    if nb is None:
+        continue
+    
+    if bcnt >= nb:
+        break
